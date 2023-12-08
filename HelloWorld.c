d76774a92964257680692c3c85146b5f417c4fdd
Rinchen Norbu Sherpa <105224211+ElliotNorbu@users.noreply.github.com>
2023-12-08 18:25:01 +0545

    Update HelloWorld.c

--- HelloWorld.c
+++ HelloWorld.c
@@ -6,12 +6,14 @@ int main(){
     int balance=200000;
     int correctPin=9898;
     int attempts=3;
+    //start of loop
     while(attempts>0){printf(" enter pin %d remaining: ",attempts);
     scanf("%d",&pin);
     if(pin==correctPin){printf("Namaste  your balance is %d",balance);
     break;}
     else {printf("incorrect password");attempts--;}
      }
+    //loop escape
     if (attempts==0){printf("maximum trial chance reached");}
     return 0;
 }
